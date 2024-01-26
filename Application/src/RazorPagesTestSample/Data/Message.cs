2a30b038f08b91c7c4ebe7c42f439379474e9c17
Erwan Herfroy <erwanh@live.com>
2024-01-26 09:48:01 +0100

    Resolves 250 char messge issue

--- Application/src/RazorPagesTestSample/Data/Message.cs
+++ Application/src/RazorPagesTestSample/Data/Message.cs
@@ -5,11 +5,12 @@ namespace RazorPagesTestSample.Data
     #region snippet1
     public class Message
     {
+        public const MessageMaxLength = 250;
         public int Id { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(MessageMaxLength, ErrorMessage = $"There's a {MessageMaxLength} character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion
